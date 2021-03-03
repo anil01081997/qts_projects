e07d24da754112b7114db1262678933a1b53058c
Anil Kumar Sahoo <anil@kredily.com>
2021-03-03 16:58:13 +0530

    initial commit

---
+++ anil_py/py_main.py
@@ -0,0 +1,2 @@
+def even_odd(test_list):
+    print("Even numbers in the list is " + ", ".join(str(data) for data in test_list if data % 2 == 0))
---
+++ main.py
@@ -0,0 +1,4 @@
+from anil_py.py_main import even_odd
+
+test_input = [12, 34, 45, 83, 342, 21, 435, 234, 12, 532, 12, 45]
+even_odd(test_input)
