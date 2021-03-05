9dba795da70afc30f2f81b15c66424829cb92ebb
Anil Kumar Sahoo <anil@kredily.com>
2021-03-05 12:04:54 +0530

    assignments added

--- anil_py/py_main.py
+++ anil_py/py_main.py
@@ -1,3 +1,6 @@
+print("\nSolution By Master-----------")
+
+
 def even_odd(test_list):
     print("Even numbers in the list are " + ", ".join(str(data) for data in test_list if data % 2 == 0))
     print("Odd numbers in the list are " + ", ".join(str(data) for data in test_list if data % 2 == 1))
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from anil_py.py_main import even_odd
+from assignments import assign1_input
 
-test_input = [12, 34, 45, 83, 342, 21, 435, 234, 12, 532, 12, 45]
-even_odd(test_input)
+
+def all_assignments():
+    print("-" * 50 + " Assignment - 1 " + "-" * 50)
+    from anil_py.py_main import even_odd
+    even_odd(assign1_input)
+
+
+all_assignments()
