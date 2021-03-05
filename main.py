8eeb7011624e2eaa8507df8e4afbdc3b4c0bdb21
Anil Kumar Sahoo <anil@kredily.com>
2021-03-05 12:12:46 +0530

    assignments added

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from assignments import assign1_input
 
 
-def all_assignments():
-    print("-" * 50 + " Assignment - 1 " + "-" * 50)
-    from anil_py.py_main import even_odd
-    even_odd(assign1_input)
-
-
-all_assignments()
+print("-" * 50 + " Assignment - 1 " + "-" * 50)
+from anil_py.py_main import even_odd
+even_odd(assign1_input)
