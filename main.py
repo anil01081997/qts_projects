d332f38314a832566c0f16ac4b120c02c924f91b
sourav <souravmahato7643@gmail.com>
2021-03-07 11:53:42 +0530

    new code added

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from assignments import assign1_input
+from assignments import assign1_input, assign2_input_2, assign2_input_1
 
 
 def first_assignment():
     print("-" * 50 + " Assignment - 1 " + "-" * 50)
     from anil_py.py_main import even_odd
     even_odd(assign1_input)
+    from sourav_py.py_main import firstAssignment
+    firstAssignment(assign1_input)
 
 
 def second_assignment():
     print("-" * 50 + " Assignment - 2 " + "-" * 50)
+    from sourav_py.py_main import secondAssign
+    secondAssign(assign2_input_1, assign2_input_2)
 
 
 first_assignment()
--- sourav_py/py_main.py
+++ sourav_py/py_main.py
@@ -1,8 +1,9 @@
 def firstAssignment(assign1_input):
+    print('solution by sourav' + "-" * 50)
     even_str = "Even numbers are:"
     odd_str = "Odd numbers are:"
-    even_list=[]
-    odd_list=[]
+    even_list = []
+    odd_list = []
     for num in assign1_input:
         if num % 2 == 0:
             even_list.append(str(num))
@@ -10,13 +11,13 @@ def firstAssignment(assign1_input):
             odd_list.append(str(num))
     print(even_str)
     print(odd_str)
-def secondAssign(assign2_input_1,x):
 
+
+def secondAssign(assign2_input_1, x):
+    print('2nd solution by sourav' + "-" * 50)
     count = 0
 
     for lest in assign2_input_1:
         if (lest == x):
             count = count + 1
     print("the number of occurrences of x is:-", count)
-
-
