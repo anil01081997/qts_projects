0d48dc8bcd886a8748933bbc0c5fdc080979e8fb
Anil Kumar Sahoo <anil@kredily.com>
2021-03-07 11:10:45 +0530

    refactored

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from assignments import assign1_input
+from assignments import assign1_input, assign2_input_2, assign2_input_1
 
 
 def first_assignment():
@@ -6,9 +6,14 @@ def first_assignment():
     from anil_py.py_main import even_odd
     even_odd(assign1_input)
 
+    from pallavi_py.py_main import assign_1
+    assign_1(assign1_input)
+
 
 def second_assignment():
     print("-" * 50 + " Assignment - 2 " + "-" * 50)
+    from pallavi_py.py_main import assign_2
+    assign_2(assign2_input_1, assign2_input_2)
 
 
 first_assignment()
