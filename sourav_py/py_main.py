ec5c294418782964043b6fd587a6667a54664479
sourav <souravmahato7643@gmail.com>
2021-03-07 11:27:38 +0530

    code added

---
+++ sourav_py/py_main.py
@@ -0,0 +1,22 @@
+def firstAssignment(assign1_input):
+    even_str = "Even numbers are:"
+    odd_str = "Odd numbers are:"
+    even_list=[]
+    odd_list=[]
+    for num in assign1_input:
+        if num % 2 == 0:
+            even_list.append(str(num))
+        else:
+            odd_list.append(str(num))
+    print(even_str)
+    print(odd_str)
+def secondAssign(assign2_input_1,x):
+
+    count = 0
+
+    for lest in assign2_input_1:
+        if (lest == x):
+            count = count + 1
+    print("the number of occurrences of x is:-", count)
+
+
