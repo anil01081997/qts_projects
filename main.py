61e44566fdbd35cdc9929c6cf00b33f962b2b2fc
Pallavi <lavikulshrestha37910@gmail.com>
2021-03-08 17:32:55 +0530

    new code

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from assignments import assign1_input
-
+from assignments import assign1_input, assign2_input_1,assign2_input_2
 
 def first_assignment():
-    print("-" * 50 + " Assignment - 1 " + "-" * 50)
-    from anil_py.py_main import even_odd
-    even_odd(assign1_input)
-
+    # print("-" * 50 + " Assignment - 1 " + "-" * 50)
+    # from anil_py.py_main import even_odd
+    # even_odd(assign1_input)
+    from pallavi_py.py_main import assignment_1
+    assignment_1(assign1_input)
 
 def second_assignment():
-    print("-" * 50 + " Assignment - 2 " + "-" * 50)
+    # print("-" * 50 + " Assignment - 2 " + "-" * 50)
+    from pallavi_py.py_main import assignment_2
+    assignment_2(assign2_input_1,assign2_input_2)
 
 
 first_assignment()
--- pallavi_py/py_main.py
+++ pallavi_py/py_main.py
@@ -1,14 +1,16 @@
 from assignments import assign1_input
-def func(list_data):
+def assignment_1(list_data):
+    print("Solution of assignment 1 by pallavi"+"-"*50)
     even = [i for i in list_data if i % 2 == 0]
     odd = [i for i in list_data if i % 2 == 1]
 
     print("Even numbers are", even)
     print("Odd numbers are", odd)
 
-def func2(list_2,num):
+def assignment_2(list_2,num):
+    print("Solution of assignment 2 by pallavi"+"-"*50)
     count=0
     for i in list_2:
         if i == num:
             count= count + 1
-    return count
+    print(count)
