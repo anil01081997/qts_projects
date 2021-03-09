adc4a4b17fa841fe4370862627b124d65d285440
Anil Kumar Sahoo <anil@kredily.com>
2021-03-09 09:31:54 +0530

    code allignments

--- pratiksha_py/py_main.py
+++ pratiksha_py/py_main.py
@@ -1,27 +1,29 @@
 def assignment1(n):
-    even_str="Even numbers are: "
-    odd_str="Odd numbers are: "
-    even_list=list()
-    odd_list=list()
+    even_str = "Even numbers are: "
+    odd_str = "Odd numbers are: "
+    even_list = list()
+    odd_list = list()
     for i in n:
-        if i%2==0:
+        if i % 2 == 0:
             even_list.append(str(i))
         else:
             odd_list.append(str(i))
     print(even_str + (' , ').join(even_list))
     print(odd_str + (' , ').join(odd_list))
 
-def assignment2(list,n):
-    print("slolution of assignment 2 by Pratiksha"+"-"*50)
-    count=0
+
+def assignment2(list, n):
+    print("slolution of assignment 2 by Pratiksha" + "-" * 50)
+    count = 0
     for i in list:
-        if i==n:
-            count=count+1
+        if i == n:
+            count = count + 1
     print(count)
+
 
 def assignment3(n):
     print("slolution of assignment 3 by Pratiksha" + "-" * 50)
-    for i in range(1,n+1):
-        for j in range(i,n):
-            print(i,end=' ')
+    for i in range(1, n + 1):
+        for j in range(i, n):
+            print(i, end=' ')
         print()
