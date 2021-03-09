3ecc4e85bdb7f01e036a9b9e84dab6737f759eee
Pallavi <lavikulshrestha37910@gmail.com>
2021-03-09 09:34:02 +0530

    completed

--- pallavi_py/py_main.py
+++ pallavi_py/py_main.py
@@ -25,7 +25,7 @@ def assignment_2(list_2,num):
 
 def assignment_3(n):
     print("Solution of assignment 3 by pallavi"+"-"*50)
-    for i in range(1,n+1):
-        for j in range(i,n):
+    for i in range(1,n+2):
+        for j in range(i,n+1):
             print(i,end=' ')
         print()
